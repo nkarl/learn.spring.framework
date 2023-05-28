1a0633a2cecf4b1aaf416196a9acd6febfb987e0
nkarl <21993921+nkarl@users.noreply.github.com>
2023-05-27 21:56:11 -0700

    Introduced annotations @Bean, @Primary, @Qualifier to resolve collision with multiple candidate beans.

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -2,6 +2,8 @@ package com.in28minutes.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
         // Steps:
@@ -11,8 +13,19 @@ public class App02HelloWorldSpring {
 
         //  2. Configure the things that we want Spring to manage.
         //      - we use @Configuration
-        System.out.println(
-                context.getBean("name")
-        );
+
+        //  3. Retrieving Beans managed by Spring
+        System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
+        System.out.println(context.getBean("person4Parameters"));
+        System.out.println(context.getBean("person5Parameters"));
+        System.out.println(context.getBean("address2"));
+        System.out.println(context.getBean(Person.class));
+        System.out.println(context.getBean(Address.class));
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -1,7 +1,15 @@
 package com.in28minutes.learnspringframework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+record Person(String name, int age, Address address) {
+}
+
+record Address(String firstLine, String city) {
+}
 
 @Configuration
 public class HelloWorldConfiguration {
@@ -9,5 +17,47 @@ public class HelloWorldConfiguration {
     @Bean
     public String name() {
         return "Karl";
+    }
+
+    @Bean
+    public int age() {
+        return 30;
+    }
+
+    @Bean
+    @Primary
+    public Person person() {
+        return new Person("Alina", 20, new Address("Main Street", "Utrecht"));
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+        return new Person(name, age, address3);
+    }
+
+    @Bean
+    public Person person4Parameters(String name, int age, Address address) {
+        return new Person(name, age, address);
+    }
+
+    @Bean
+    public Person person5Parameters(String name, int age, @Qualifier("address3qualifier") Address address) {
+        return new Person(name, age, address);
+    }
+    @Bean(name = "address2")
+    @Primary
+    public Address address() {
+        return new Address("Baker Street", "London");
+    }
+
+    @Bean(name = "address3")
+    @Qualifier("address3qualifier")
+    public Address address3() {
+        return new Address("Stadium Way", "Pullman");
     }
 }
