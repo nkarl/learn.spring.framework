4ac9a238cc7fd711d6f99dd8b2de28b04f57f8bf
nkarl <21993921+nkarl@users.noreply.github.com>
2023-05-27 22:09:03 -0700

    Applied Beans to game context examples.

---
+++ src/main/java/com/in28minutes/learnspringframework/GamingConfiguration.java
@@ -0,0 +1,23 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.awt.image.PackedColorModel;
+
+@Configuration
+public class GamingConfiguration {
+    @Bean
+    public GamingConsole game() {
+        return new PacmanGame();
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
+    }
+}
