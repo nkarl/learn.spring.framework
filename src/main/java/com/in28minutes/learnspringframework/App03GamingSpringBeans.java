eb5b1dd0cf9293442a56d63b7e2e94fe0e3c72ed
nkarl <21993921+nkarl@users.noreply.github.com>
2023-05-28 17:22:15 -0700

    Updated.

--- src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -1,9 +1,9 @@
 package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.GamingConsole;
-import com.in28minutes.learnspringframework.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
 
 public class App03GamingSpringBeans {
     public static void main(String[] args) {
@@ -12,8 +12,11 @@ public class App03GamingSpringBeans {
                      new AnnotationConfigApplicationContext(
                              GamingConfiguration.class
                      )) {
-            context.getBean(GamingConsole.class).up();
+//            context.getBean(GamingConsole.class).up();
+            context.getBean("game");
+            context.getBean("game2");
             context.getBean(GameRunner.class).run();
+            context.getBean(GameRunner.class, "game2").run();
         }
     }
 }
--- src/main/java/com/in28minutes/learnspringframework/GamingConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/GamingConfiguration.java
@@ -6,18 +6,26 @@ import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.PacmanGame;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.awt.image.PackedColorModel;
 
 @Configuration
 public class GamingConfiguration {
+
     @Bean
+    @Primary
     public GamingConsole game() {
-        return new PacmanGame();
+        return new PacmanGame(); // up down left right
     }
 
     @Bean
-    public GameRunner gameRunner(GamingConsole game) {
-        return new GameRunner(game);
+    public GamingConsole game2() {
+        return new MarioGame(); // jump down back accelerate
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole someGame) {
+        return new GameRunner(someGame); // Spring auto-wires from any existing bean it can find.
     }
 }
